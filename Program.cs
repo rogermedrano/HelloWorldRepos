1ef365393eafa27f0669f2798b24ca51ece741fe
Roger Medrano <email@example.com>
2021-08-18 12:26:46 -0700

    1st commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!  What is your favorite animal?");
+            var favAnimal = Console.ReadLine();
+            Console.WriteLine("Well, I see " + favAnimal "is your favorite animal.")
+        }
+    }
+}
